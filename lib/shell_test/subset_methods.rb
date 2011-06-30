4510d11a7b022f8f5d9074121b6e219649b415c5
Simon Chiang <simon.a.chiang@gmail.com>
2011-06-30 17:17:53 -0600

    stripped SubsetMethods to only support conditions

--- lib/shell_test/subset_methods.rb
+++ lib/shell_test/subset_methods.rb
@@ -3,51 +3,21 @@ require 'benchmark'
 module ShellTest
 
   # SubsetMethods provides methods to conditionally run tests.
-  #  
-  #   require 'shell_test'
-  #
-  #   class Test::Unit::TestCase
-  #     # only true if running on windows
-  #     condition(:windows) { match_platform?('mswin') }
-  #  
-  #     # only true if running on anything but windows
-  #     condition(:non_windows) { match_platform?('non_mswin') }
-  #   end
-  #
-  # Here the WindowsOnlyTest will only run on a Windows platform. Conditions
-  # like these may be targeted at specific tests when only some tests need
-  # to be skipped.
-  #  
-  #   class RunOnlyAFewTest < Test::Unit::TestCase
-  #     include SubsetMethods
-  #  
-  #     def test_runs_all_the_time
-  #       assert true
-  #     end
-  #  
-  #     def test_runs_only_if_non_windows_condition_is_true
-  #       condition_test(:non_windows) { assert true }
-  #       end
-  #     end
-  #  
-  #     def test_runs_only_when_ENV_variable_EXTENDED_is_true
-  #       extended_test { assert true }
-  #     end
-  #  
-  #     def test_runs_only_when_ENV_variable_BENCHMARK_is_true
-  #       benchmark_test do |x|
-  #         x.report("init speed") { 10000.times { Object.new } }
-  #       end
-  #     end
-  #
-  #     def test_runs_only_when_ENV_variable_CUSTOM_is_true
-  #       subset_test('CUSTOM') { assert true }
+  # 
+  #   require 'shell_test/unit'
+  #   
+  #   class ShellTestExample < Test::Unit::TestCase
+  #     include ShellTest::SubsetMethods
+  #     condition(:never_run)   { false }
+  #   
+  #     def test_using_conditions
+  #       condition_test(:never_run) { flunk }
   #     end
   #   end
-  #  
-  # In the example, the ENV variables EXTENDED, BENCHMARK, and CUSTOM act as
-  # flags to run specific tests.  See SubsetMethods::ClassMethods for more
-  # information.
+  #
+  # Any kind of conditions may be checked by the block - if it returns truthy
+  # then the condition test will run.  Note the condition block is evaluated
+  # every time the condition is used in a test.
   module SubsetMethods
     # Class methods associated with SubsetTest.
     module ClassMethods
@@ -113,31 +83,6 @@ module ShellTest
         end
         unsatified
       end
-
-      # Returns true if RUBY_PLATFORM matches one of the specfied
-      # platforms.  Use the prefix 'non_' to specify any plaform 
-      # except the specified platform (ex: 'non_mswin').  Returns
-      # true if no platforms are specified.
-      #
-      # Some common platforms:
-      #   mswin    Windows
-      #   darwin   Mac
-      def match_platform?(*platforms)
-        platforms.each do |platform|
-          platform.to_s =~ /^(non_)?(.*)/
-
-          non = true if $1
-          match_platform = !RUBY_PLATFORM.index($2).nil?
-          return false unless (non && !match_platform) || (!non && match_platform)
-        end
-
-        true
-      end
-
-      # Returns true if type or 'ALL' is specified as 'true' in ENV.
-      def run_subset?(type)
-        ENV[type] == "true" || ENV['ALL'] == "true" ? true : false
-      end
     end
 
     def self.included(base) # :nodoc:
@@ -148,38 +93,6 @@ module ShellTest
     # Returns true if the specified conditions are satisfied.
     def satisfied?(*condition_names)
       self.class.satisfied?(*condition_names)
-    end
-
-    # Returns true if the subset type (ex 'BENCHMARK') or 'ALL' is
-    # specified in ENV.
-    def run_subset?(type)
-      self.class.run_subset?(type)
-    end
-
-    # Returns true if the input string matches the regexp specified in 
-    # ENV[type]. Returns the default value if ENV['ALL'] is 'true', and the
-    # default if type is not specified in ENV.
-    def match_regexp?(type, str, default=true)
-      return true if ENV["ALL"] == 'true'
-      return default unless value = ENV[type]
-
-      str =~ Regexp.new(value) ? true : false
-    end
-
-    # Platform-specific test.  Useful for specifying test that should only 
-    # be run on a subset of platforms.  Prints ' ' if the test is not run.
-    #
-    #   def test_only_on_windows
-    #     platform_test('mswin') { ... }
-    #   end
-    #
-    # See SubsetMethodsClass#match_platform? for matching details.
-    def platform_test(*platforms)
-      if self.class.match_platform?(*platforms)
-        yield
-      else
-        print ' '
-      end
     end
 
     # Conditonal test.  Only runs if the specified conditions are satisfied.
@@ -199,88 +112,10 @@ module ShellTest
     #
     # See SubsetMethodsClass#condition for more details.
     def condition_test(*condition_names)
-      if self.class.unsatisfied_conditions(*condition_names).empty?
+      if satisfied?(*condition_names)
         yield
       else
         print ' '
-      end
-    end
-
-    # Defines a subset test.  The provided block will only run if:
-    # - the subset type is specified in ENV
-    # - the subset 'ALL' is specified in ENV
-    # - the test method name matches the regexp provided in the 
-    #   <TYPE>_TEST ENV variable
-    #  
-    # Otherwise the block will be skipped and +skip+ will be printed in the
-    # test output.  By default skip is the first letter of +type+.
-    #
-    # For example, with these methods:
-    #
-    #   def test_one
-    #     subset_test('CUSTOM') { assert true }
-    #   end
-    #
-    #   def test_two
-    #     subset_test('CUSTOM') { assert true }
-    #   end
-    #
-    #   Condition                    Tests that get run
-    #   ENV['ALL']=true              test_one, test_two
-    #   ENV['CUSTOM']=true           test_one, test_two
-    #   ENV['CUSTOM_TEST']=test_     test_one, test_two
-    #   ENV['CUSTOM_TEST']=test_one  test_one
-    #   ENV['CUSTOM']=nil            no tests get run
-    # 
-    # If you're running your tests with rake (or rap), ENV variables may be
-    # set from the command line.
-    #
-    #   # all tests
-    #   % rap test all=true
-    #
-    #   # custom subset tests
-    #   % rap test custom=true
-    #
-    #   # just test_one (it is the only test
-    #   # matching the '.est_on.' pattern)
-    #   % rap test custom_test=.est_on.
-    #
-    def subset_test(type, skip=type[0..0].downcase)
-      type = type.upcase
-      if run_subset?(type) || ENV["#{type}_TEST"]
-        if match_regexp?("#{type}_TEST", __name__.to_s)
-          yield
-        else
-          print skip
-        end
-      else
-        print skip
-      end
-    end
-
-    # Declares a subset_test for the ENV variable 'EXTENDED'.
-    # Prints 'x' if the test is not run.
-    #
-    #   def test_some_long_process
-    #     extended_test { ... }
-    #   end
-    def extended_test(&block) 
-      subset_test("EXTENDED", "x", &block)
-    end
-
-    # Declares a subset_test for the ENV variable 'BENCHMARK'. If run, 
-    # benchmark_test sets up benchmarking using the Benchmark.bm method 
-    # using the input length and block.  Prints 'b' if the test is not run.
-    #
-    #   include Benchmark
-    #   def test_speed
-    #     benchmark_test(10) {|x| ... }
-    #   end
-    def benchmark_test(length=10, &block) 
-      subset_test("BENCHMARK") do
-        puts
-        puts __name__
-        Benchmark.bm(length, &block)
       end
     end
   end
--- test/shell_test/subset_methods_test.rb
+++ test/shell_test/subset_methods_test.rb
@@ -4,92 +4,6 @@ require 'shell_test/subset_methods'
 class SubsetMethodsTest < Test::Unit::TestCase
   include ShellTest::SubsetMethods
 
-  def setup
-    @env_hold = {}
-    ENV.each_pair do |key, value|
-      @env_hold[key] = value
-    end
-    ENV.clear
-  end
-
-  def teardown
-    ENV.clear
-    @env_hold.each_pair do |key, value|
-      ENV[key] = value
-    end
-  end
-
-  #
-  # subset_test test
-  #
-
-  def test_subset_test_normally_does_not_run
-    was_in_block = false
-    subset_test('type') do 
-      was_in_block = true
-    end
-
-    assert_equal false, was_in_block
-  end
-
-  def test_subset_test_runs_if_ENV_TYPE_is_true
-    ENV['TYPE'] = "true"
-
-    was_in_block = false
-    subset_test('type') do 
-      was_in_block = true
-    end
-
-    assert_equal true, was_in_block
-  end
-
-  def test_subset_test_runs_if_ENV_ALL_is_true
-    ENV['ALL'] = "true"
-
-    was_in_block = false
-    subset_test('type') do 
-      was_in_block = true
-    end
-
-    assert_equal true, was_in_block
-  end
-
-  def test_subset_test_runs_if_ENV_TYPE_TEST_matches_current_test
-    ENV['TYPE_TEST'] = "ENV_TYPE_TEST_matches"
-
-    was_in_block = false
-    subset_test('type') do 
-      was_in_block = true
-    end
-
-    assert_equal true, was_in_block
-  end
-
-  def test_subset_test_does_not_run_if_ENV_TYPE_TEST_does_not_match_current_test
-
-    ENV['TYPE_TEST'] = "run_subset_true_if_ENV_type_test_matches_current"
-    was_in_block = false
-    subset_test('type') do 
-      was_in_block = true
-    end
-
-    assert_equal false, was_in_block
-  end
-
-  #
-  # extended_test test
-  #
-
-  def test_extended_test_runs_only_if_extended_true
-    was_in_test = false
-    extended_test { was_in_test = true }
-    assert !was_in_test
-
-    ENV['EXTENDED'] = 'true'
-    extended_test { was_in_test = true }
-    assert was_in_test
-  end
-
   #
   # condition test
   #
@@ -110,43 +24,6 @@ class SubsetMethodsTest < Test::Unit::TestCase
     condition_test(:is_true, :is_false) { test_ran = true}
     assert !test_ran
   end
-
-  #
-  # run subset test
-  #
-
-  def test_run_subset_true_if_ENV_type_is_true
-    assert_equal false, run_subset?('type')
-
-    ENV['type'] = "true"
-    assert_equal true, run_subset?('type')
-  end
-
-  def test_run_subset_true_if_ENV_all_is_true
-    ENV['ALL'] = "true"
-    assert_equal true, run_subset?('type')
-  end
-
-  #
-  # match_regexp test
-  #
-
-  def test_match_regexp
-    ENV['type'] = "one.*"
-    assert_equal true, match_regexp?('type', "one")
-    assert_equal true, match_regexp?('type', "one_two")
-    assert_equal true, match_regexp?('type', "two_one")
-    assert_equal false, match_regexp?('type', "non")
-  end
-
-  def test_match_regexp_returns_true_if_ENV_all_true
-    ENV['ALL'] = "true"
-    assert_equal true, match_regexp?('type', "str")
-  end
-
-  def test_match_regexp_returns_default_unless_ENV_type_is_set
-    assert_equal false, match_regexp?('type', "str", false)
-  end
 end
 
 #
@@ -165,7 +42,7 @@ class SubsetBaseTest < Test::Unit::TestCase
   end
 
   def test_class_level_condition
-    assert self.class.satisfied?(:satisfied)
+    assert satisfied?(:satisfied)
   end
 end
 
@@ -175,7 +52,7 @@ class SubsetInheritanceTest < SubsetBaseTest
   end
 
   def test_class_level_condition
-    assert self.class.satisfied?(:satisfied)
+    assert satisfied?(:satisfied)
   end
 end
 
@@ -189,6 +66,19 @@ class SubsetOverrideTest < SubsetBaseTest
   end
 
   def test_class_level_condition
-    assert !self.class.satisfied?(:satisfied)
+    assert !satisfied?(:satisfied)
+  end
+end
+
+#
+# Documentation Tests
+#
+
+class ShellTestExample < Test::Unit::TestCase
+  include ShellTest::SubsetMethods
+  condition(:never_run)   { false }
+
+  def test_using_conditions
+    condition_test(:never_run) { flunk }
   end
 end
