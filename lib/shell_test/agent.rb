07f95926b5177683a45da282321eaef47da2e38d
Simon Chiang <simon.a.chiang@gmail.com>
2011-09-18 16:16:36 -0600

    documentation of Agent

--- lib/shell_test/agent.rb
+++ lib/shell_test/agent.rb
@@ -1,8 +1,13 @@
 require 'shell_test/timeout_timer'
+require 'pty'
 
 module ShellTest
   class Agent
     class << self
+      # Spawns a PTY session and yields an Agent for that session to the
+      # block. Run ensures the PTY process is killed upon errors, and but
+      # re-raises the error for additional handling.  Lastly, run sets the
+      # command status to $? upon completion.
       def run(cmd)
         PTY.spawn(cmd) do |slave, master, pid|
           begin
@@ -17,8 +22,13 @@ module ShellTest
       end
     end
 
+    # The pty master
     attr_reader :master
+
+    # The pty slave
     attr_reader :slave
+
+    # A timer tracking timeouts up to a maximum run time
     attr_reader :timer
 
     def initialize(master, slave, timer = TimeoutTimer.new)
@@ -27,28 +37,51 @@ module ShellTest
       @timer  = timer
     end
 
+    # Starts a run by setting the timer to timeout after max_run_time seconds.
     def start_run(max_run_time)
       timer.start(max_run_time)
     end
 
-    # Returns time elapsed
+    # Stops the timer and returns the elapsed since start_run.
     def stop_run
       timer.stop
     end
 
-    # Timeout:
+    # Reads from the slave until the regexp is matched and returns the
+    # resulting string.  If a nil regexp is given then expect reads until the
+    # slave eof.
     #
-    #   pos - sets mark at current time + timeout (up to max_run_time)
-    #   neg - preserves current mark
-    #   nil - sets mark to end time
+    # A timeout may be given.  If the slave doesn't produce the expected
+    # string (or eof) within the timeout then expect raises an error.
     #
-    def expect(prompt, timeout=nil)
+    # ==== Timeout
+    #
+    # The timeout is designed to allow incremental timeouts in a multi-step
+    # sessions, bounded by a maximum run time (see start_run).  As such the
+    # timeout actually defines a mark and the effective timeout is the time to
+    # that mark.
+    #
+    #   timeout value   effect
+    #   positive        sets mark at current time + timeout (up to max_run_time)
+    #   negative        preserves current mark
+    #   nil             sets mark to end time
+    #
+    # ==== Partial Length
+    #
+    # A larger partial length may be specified to speed up expect when the
+    # regexp is intended to match strings at a point where the slave will run
+    # out of data, for example prompts where the pty waits for input.
+    #
+    # Note that used inappropriately this may result in more data being read
+    # from the slave than is necessary to match the regexp.
+    #
+    def expect(regexp, timeout=nil, partial_len=1)
       timer.set_timeout(timeout)
 
       buffer = ''
       while true
         if !IO.select([slave],nil,nil,timer.timeout)
-          raise TimeoutError, "waiting for: #{prompt.inspect}\n#{buffer.inspect}"
+          raise TimeoutError, "waiting for: #{regexp.inspect}\n#{buffer}"
         end
 
         if slave.eof?
@@ -60,15 +93,16 @@ module ShellTest
         #
         # Use readpartial+select instead of read_nonblock to avoid polling in
         # a tight loop.
-        buffer << slave.readpartial(1024)
+        buffer << slave.readpartial(partial_len)
 
-        if prompt && buffer =~ prompt
+        if regexp && buffer =~ regexp
           break
         end
       end
       buffer
     end
 
+    # Writes to the master.
     def write(input)
       master.print input
     end
