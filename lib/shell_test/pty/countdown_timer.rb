c6d9ded00376acf8650d3683ef5582e016b4dbb6
Simon Chiang <simon.a.chiang@gmail.com>
2011-09-24 10:07:51 -0600

    ensure countdown timer uses numeric times

--- lib/shell_test/pty/countdown_timer.rb
+++ lib/shell_test/pty/countdown_timer.rb
@@ -3,8 +3,8 @@ module ShellTest
     # A timer to provide timeouts counting down to a specified stop time.
     class CountdownTimer
 
-      # A clock used to determine now - normally the clock is simply Time, but a
-      # different clock can be provided as needed.
+      # A clock used to determine current_time - normally the clock is simply
+      # Time, but a different clock can be provided as needed.
       attr_reader :clock
 
       # The specified stop time
@@ -15,19 +15,25 @@ module ShellTest
         @stop_time = 0
       end
 
-      # Sets the stop time to be now plus the specified timeout.
-      def timeout=(timeout)
-        @stop_time = timeout.nil? ? nil : clock.now + timeout
+      # Returns the current time as a Float.
+      def current_time
+        clock.now.to_f
       end
 
-      # Returns the duration from now until the stop time.  If that duration is
-      # less than zero, then 0 is returned.  If the stop time is nil, then
-      # timeout returns nil.
+      # Sets the stop time to be current_time plus the specified numeric
+      # timeout.
+      def timeout=(timeout)
+        @stop_time = timeout.nil? ? nil : current_time + timeout
+      end
+
+      # Returns the duration from current_time until stop_time.  If that
+      # duration is less than zero, then 0 is returned.  If the stop_time is
+      # nil, then timeout returns nil.
       def timeout
         if stop_time.nil?
           nil
         else
-          timeout = stop_time - clock.now
+          timeout = stop_time - current_time
           timeout < 0 ? 0 : timeout
         end
       end
