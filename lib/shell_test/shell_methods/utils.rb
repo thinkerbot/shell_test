e1faf9ef2004922744d41a0fcb2bb82df0e85205
Simon Chiang <simon.a.chiang@gmail.com>
2011-10-17 19:37:13 -0600

    note 1.8.7 PTY race condition in documentation

--- lib/shell_test/shell_methods/utils.rb
+++ lib/shell_test/shell_methods/utils.rb
@@ -9,9 +9,11 @@ module ShellTest
       # upon completion. The PTY process is killed upon an unhandled error
       # (but the error is re-raised for further handling).
       #
-      # Note that $? is not a reliable way to determine the status for the
-      # session (and AFAIK cannot be made reliable in 1.8.7) - rely on the
-      # output of spawn.
+      # Note that $? is set by spawn but is not reliable until 1.9.2 (ish).
+      # Prior to that PTY used a cleanup thread that would wait on a spawned
+      # process and raise a PTY::ChildExited error in some cases.  As a result
+      # manual calls to Process.wait (which would set $?) cause a race
+      # condition. Rely on the output of spawn instead.
       def spawn(cmd)
         PTY.spawn(cmd) do |slave, master, pid|
           begin
@@ -19,16 +21,16 @@ module ShellTest
             Process.wait(pid)
 
           rescue PTY::ChildExited
-            # wait (and maybe other stuff) can cause a ChildExited error on
-            # 1.8.6 and 1.8.7 as a 'normal' exit route.  1.9.2 does not exit
-            # this way.
+            # handle a ChildExited error on 1.8.6 and 1.8.7 as a 'normal' exit
+            # route.  1.9.2 does not exit this way.
             return $!.status
 
           rescue Exception
-            # Cleanup the pty on error
+            # cleanup the pty on error
             Process.kill(9, pid)
 
-            # Any wait can cause a ChildExited error so account for that here.
+            # any wait can cause a ChildExited error so account for that here
+            # - the $? is indeterminate in this case prior to 1.9.2
             Process.wait(pid) rescue PTY::ChildExited
 
             raise
