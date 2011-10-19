0d106c1b22f1cfbcf6a42cf7e6750490eede5a38
Simon Chiang <simon.chiang@pinnacol.com>
2011-10-18 19:34:17 -0600

    require fileutils where used

--- test/shell_test/shell_methods_test.rb
+++ test/shell_test/shell_methods_test.rb
@@ -1,5 +1,6 @@
 require File.expand_path('../../test_helper', __FILE__)
 require 'shell_test/shell_methods'
+require 'fileutils'
 
 class ShellMethodsTest < Test::Unit::TestCase
   include ShellTest::ShellMethods
