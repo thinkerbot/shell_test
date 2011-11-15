3d8bc55f1100a50c7542d7ba9cd2760193fe7d72
Simon Chiang <simon.chiang@pinnacol.com>
2011-11-15 12:57:30 -0700

    add back hack to achieve quicker exits on error

--- lib/shell_test/shell_methods/utils.rb
+++ lib/shell_test/shell_methods/utils.rb
@@ -14,6 +14,20 @@ module ShellTest
             return yield(master, slave)
           rescue Exception
             Process.kill(9, pid)
+
+            # Clearing the slave allows quicker exits on OS X.
+            while IO.select([slave],nil,nil,0.1)
+              begin
+                break unless slave.read(1)
+              rescue Errno::EIO
+                # On some linux (ex ubuntu) read can return an eof or fail with
+                # an EIO error when a terminal disconnect occurs and an EIO
+                # condition occurs - the exact behavior is unspecified but the
+                # meaning is the same... no more data is available, so break.
+                break
+              end
+            end
+
             raise
           ensure
             Process.wait(pid)
